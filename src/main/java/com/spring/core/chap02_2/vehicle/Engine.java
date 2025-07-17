ed08dccc6bbe032664b3eb1a31cc5618a2a77ed9
rlaban12 <114490382+rlaban12@users.noreply.github.com>
2025-07-17 16:37:01 +0900

    20250717_1636 commit

--- src/main/java/com/spring/core/chap02_2/vehicle/Engine.java
+++ src/main/java/com/spring/core/chap02_2/vehicle/Engine.java
@@ -2,11 +2,13 @@ package com.spring.core.chap02_2.vehicle;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class Engine {
 
+    public Engine() {
+        System.out.println("엔진이 생성됨!");
+    }
 
     public void start() {
-        System.out.println("엔진 시동 걸렸습니다.");
+        System.out.println("엔진 시동이 걸렸습니다.");
     }
 }
